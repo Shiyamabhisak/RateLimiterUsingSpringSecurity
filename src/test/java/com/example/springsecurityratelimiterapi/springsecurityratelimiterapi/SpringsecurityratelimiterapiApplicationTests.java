834a1b043583282d29cc37b9679351b973c2dde4
Shiyamabhisak <shiyamabhisak.vijayasurresh@striim.com>
2024-09-02 23:25:31 +0530

    Linking the project to git

---
+++ src/test/java/com/example/springsecurityratelimiterapi/springsecurityratelimiterapi/SpringsecurityratelimiterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityratelimiterapi.springsecurityratelimiterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityratelimiterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
